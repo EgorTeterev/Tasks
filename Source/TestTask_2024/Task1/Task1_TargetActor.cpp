a83b226738030425dd96ab7270e739fc7784fe03
Egor Teterev <egorteterevfs@gmail.com>
2024-06-07 19:29:54 +0300

    almost made 1st task,needs code refactoring

--- Source/TestTask_2024/Task1/Task1_TargetActor.cpp
+++ Source/TestTask_2024/Task1/Task1_TargetActor.cpp
@@ -15,13 +15,41 @@ void ATask1_TargetActor::BeginPlay()
 {
 	Super::BeginPlay();
 	
-	APlayerController* Controller = GetWorld()->GetFirstPlayerController();
-
+	Character = Cast<ACharacter>(UGameplayStatics::GetPlayerPawn(GetWorld(), 0));
+	TargetLocation = GetActorLocation();
 }
-//====================================================================================================================================================================
+
 void ATask1_TargetActor::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+
+	UpdateDistance();
+	IsInViewPort();
+}
+//====================================================================================================================================================================
+void ATask1_TargetActor::UpdateDistance()
+{
+	CharacterLocation = Character->GetActorLocation();
+	DistanceToCharacter = floor(FVector::Distance(TargetLocation, CharacterLocation)/100);
+}	
+
+void ATask1_TargetActor::IsInViewPort()
+{
+	//FVector2D ScreenPosition;
+	APlayerController* CharacterPlayerController = Cast<APlayerController>(Character->GetController());
+	if(!CharacterPlayerController) return;
+
+
+	CharacterPlayerController->ProjectWorldLocationToScreen(GetActorLocation(), ScreenPosition);
+
+
+	CharacterPlayerController->GetViewportSize(ViewPortSizeX, ViewPortSizeY);
+
+	bIsInViewPort = ScreenPosition.X >= 0 && ScreenPosition.X <= ViewPortSizeX && ScreenPosition.Y >= 0 && ScreenPosition.Y <= ViewPortSizeY;
+
+	ClampedPosition = ScreenPosition;
+	ClampedPosition.X = FMath::Clamp(ClampedPosition.X, 0, ViewPortSizeX);
+	ClampedPosition.Y = FMath::Clamp(ClampedPosition.Y, 0, ViewPortSizeY);
 
 }
 
--- Source/TestTask_2024/Task1/Task1_TargetActor.h
+++ Source/TestTask_2024/Task1/Task1_TargetActor.h
@@ -3,6 +3,9 @@
 #include "GameFramework/Actor.h"
 #include "ItteractInterface.h"
 #include "GameFramework/Controller.h"
+#include "Kismet/GameplayStatics.h"
+#include "GameFramework/Character.h"
+#include "Kismet/KismetMathLibrary.h"
 #include "Task1_TargetActor.generated.h"
 
 //====================================================================================================================================================================
@@ -18,9 +21,30 @@ public:
 	float DistanceToCharacter;
 	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Stats")
 	bool bIsInViewPort;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Stats")
+	FVector2D VectorToShowWidget;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Stats")
+	FVector2D ScreenPosition;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Stats")
+
+	FVector2D ClampedPosition;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Stats")
+	int32 ViewPortSizeX;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Stats")
+	int32 ViewPortSizeY;
+//target location is static,so we can check it one,on BeginPlay()
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Stats")
+	FVector TargetLocation;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Character")
+	ACharacter* Character;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Character location")
+	FVector CharacterLocation;
 	void Itteract();
 protected:
 	virtual void BeginPlay() override;
 	virtual void Tick(float DeltaTime) override;
-	
+	void UpdateDistance();
+	void IsInViewPort();
 };
